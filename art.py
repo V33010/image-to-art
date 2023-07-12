b523b70dbd2f79b277bc7eb831d1e593313418d4
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2023-07-12 08:37:36 +0000

    code cleanup

--- art.py
+++ art.py
@@ -1,22 +1,21 @@
-# install PIL: pip install Pillow
-# install numpy: pip install numpy
-
-
 from PIL import Image
 import numpy as np
 import time
 import os
 import sys
 
+
 def cool_print(string):
     x = list(string)
     for i in x:
-        print(i, end='', flush=True)
+        print(i, end="", flush=True)
         time.sleep(0.03)
+
 
 def clear_terminal():
     # Clear terminal for Windows and other platforms
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 clear_terminal()
 
@@ -26,10 +25,8 @@ masterkey = 0
 while masterkey == 0:
     key = 0
     while key == 0:
-        
         cool_print("Enter the name of the image file: ")
         x = input()
-
 
         try:
             img = Image.open(x)
@@ -43,7 +40,7 @@ while masterkey == 0:
         print()
         cool_print("\nWas the image shown correct? (y/n): ")
         y = input()
-        if y == 'y' or y == 'Y' or y == 'yes' or y == 'Yes' or y == 'YES':
+        if y == "y" or y == "Y" or y == "yes" or y == "Yes" or y == "YES":
             key = 1
             clear_terminal()
             break
@@ -51,13 +48,13 @@ while masterkey == 0:
             clear_terminal()
             continue
 
-
-    
     # print dimensions of image
     cool_print("\nCurrent dimensions of image are: ")
-    cool_print(str(img.size)) 
+    cool_print(str(img.size))
 
-    cool_print("\nEnter the new dimensions of the image (It is recommended to keep the ratio same, and to keep the largest dimension below 180)\n")
+    cool_print(
+        "\nEnter the new dimensions of the image (It is recommended to keep the ratio same, and to keep the largest dimension below 180)\n"
+    )
     key = 0
     while key == 0:
         img = Image.open(x)
@@ -80,14 +77,17 @@ while masterkey == 0:
                 continue
 
         clear_terminal()
-        
-        cool_print(f"New dimensions are {width}x{height}. The new image will be shown for confirmation")
-        img = img.resize((width, height), Image.Resampling.LANCZOS); # reduce image size
+
+        cool_print(
+            f"New dimensions are {width}x{height}. The new image will be shown for confirmation"
+        )
+        img = img.resize((width, height), Image.Resampling.LANCZOS)
+        # reduce image size
         img.show()
         print()
         cool_print("Is this image correct? (y/n): ")
         y = input()
-        if y == 'y' or y == 'Y' or y == 'yes' or y == 'Yes' or y == 'YES':
+        if y == "y" or y == "Y" or y == "yes" or y == "Yes" or y == "YES":
             key = 1
             print("")
             break
@@ -95,20 +95,16 @@ while masterkey == 0:
             clear_terminal()
             continue
 
-
     arr = np.array(img)
-
 
     for i in range(height):
         for j in range(width):
-            arr[i][j] = max(arr[i][j][0],arr[i][j][1],arr[i][j][2])
+            arr[i][j] = max(arr[i][j][0], arr[i][j][1], arr[i][j][2])
 
-    chars = ' .,:;\'`-_^/\\|!()[]<>#$@%'
+    chars = " .,:;'`-_^/\\|!()[]<>#$@%"
 
     # creat array of size width x height
     arrnew = np.empty([height, width], dtype=str)
-
-
 
     for i in range(height):
         for j in range(width):
@@ -124,17 +120,15 @@ while masterkey == 0:
 
     filename = filename + ".txt"
 
-
     # create a new file and write the array to it
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         for i in range(height):
             for j in range(width):
                 f.write(arrnew[i][j])
-                f.write(' ')
-            f.write('\n')
+                f.write(" ")
+            f.write("\n")
 
-
-    cool_print(f'\nArt saved to file {filename}\n')
+    cool_print(f"\nArt saved to file {filename}\n")
     time.sleep(3)
     clear_terminal()
 
@@ -143,7 +137,7 @@ while masterkey == 0:
     # press 1 to run again
     cool_print("Enter 1 to run again, or any other key to exit: ")
     x = input()
-    if x == '1':
+    if x == "1":
         clear_terminal()
         continue
     else:
